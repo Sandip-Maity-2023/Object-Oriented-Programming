b6ece7af702a75920bc12ff7fdfd04ed67689769
Sandip-Maity-2023 <12sandip125@gmail.com>
2025-03-09 19:01:13 +0530

    update

---
+++ File_Example.java
@@ -0,0 +1,37 @@
+import java.io.File;
+import java.io.IOException;
+
+ public class File_Example {
+public static void main(String[] args) throws IOException {
+    File x;
+    if(args.length>0){
+        for(int i=0;i<args.length;i++){
+            x=new File(args[i]);
+            getPaths(x);
+            getInfo(x);
+
+        }
+    }else
+System.out.println("Usage: Java FileExample test<filename (s)>");
+}    
+
+public static void getPaths(File f) throws IOException{
+
+System.out.println("Name: "+f.getName());
+System.out.println("Path: "+f.getPath());
+System.out.println("Parent: "+f.getParent());
+
+}
+public static void getInfo(File f) throws IOException {
+
+    if(f.exists()){
+        System.out.println("File exits");
+        System.out.println(f.canRead() ? "and is readable:" :"");
+        System.out.println(f.canWrite() ? "and is writable:" :"");
+        System.out.println("File is last modified:"+f.lastModified());
+        System.out.println("File is: "+f.length()+"bytes");
+    }else{
+        System.err.println("File does not exit.");
+    }
+} 
+ }
---
+++ helloworld.java
@@ -0,0 +1,10 @@
+
+import java.applet.*;
+import java.awt.*;
+
+public class helloworld extends Applet{
+    public void paint(Graphics g){
+        g.drawString("Hello World", 20, 20);
+    }
+    
+}
