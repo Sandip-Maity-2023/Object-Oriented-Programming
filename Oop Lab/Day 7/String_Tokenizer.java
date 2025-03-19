243840edc3332f1f01d90ef65960b984906a5053
Sandip-Maity-2023 <12sandip125@gmail.com>
2025-03-19 22:40:22 +0530

    update

--- Oop Lab/Day 7/String_Tokenizer.java
+++ Oop Lab/Day 7/String_Tokenizer.java
@@ -1,7 +1,7 @@
 import java.util.StringTokenizer;
  public class String_Tokenizer {
 public static void main(String[] args) {
-	String s="helllo how are you";
+	String s="hello how are you";
 	StringTokenizer x=new StringTokenizer(s," ");
 	while(x.hasMoreTokens()) {
 		System.out.println(x.nextToken());
